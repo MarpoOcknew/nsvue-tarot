5353f26e9ec8662ae98abb0c7a3eca29c69c07ad
Martin Pollock <martin@viacreative.co.uk>
2020-07-23 15:41:43 +0100

    Initial commit

---
+++ app/components/CardOfTheDay.vue
@@ -0,0 +1,64 @@
+<template>
+    <GridLayout rows="auto,auto" verticalAlignment="top">
+        <Label row="0" text="Card of the Day" class="title med" />
+
+        <GridLayout row="1" rows="2*,*,3*,3*" class="card">
+            <Label row="0" textWrap="true" class="card-title" :text="name" />
+
+            <StackLayout v-if="reversed" row="1" style="margin:5"
+                verticalAlignment="top" horizontalAlignment="center"
+                orientation="horizontal">
+                <Label class="fa status" text.decode="&#xf021;" />
+                <Label class="status" text=" REVERSED" />
+            </StackLayout>
+
+            <Image row="2" v-if="major" :class="icon" :src="emoji" />
+
+            <StackLayout row="2" v-if="!major" horizontalAlignment="center"
+                orientation="horizontal">
+                <Image style="margin:5" v-if="!major" :class="icon"
+                    :src="emoji1" />
+                <Image style="margin:5" v-if="!major" :class="icon"
+                    :src="emoji2" />
+            </StackLayout>
+
+            <Label row="3" class="meaning" textWrap="true" :text="meaning" />
+        </GridLayout>
+    </GridLayout>
+</template>
+
+<script>
+    import { Cards } from "../data/cards";
+    import Tarot from "./tarot";
+
+    export default {
+        mixins: [Tarot],
+
+        methods: {
+            getCard(context) {
+                let sNum = Math.floor(Math.random() * 2);
+                let rNum = Math.round(Math.random() * 72);
+                this.getOneCard(context, sNum, rNum);
+            }
+        },
+
+        created() {
+            this.getCard("day");
+        }
+    };
+</script>
+
+<style scoped>
+    .meaning {
+        font-size: 16;
+        color: #131636;
+        text-align: center;
+        margin: 0 20;
+    }
+
+    .status {
+        color: #323340;
+        opacity: .5;
+        font-size: 14;
+    }
+</style>
